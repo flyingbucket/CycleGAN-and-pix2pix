4a3fcb1026b1d74fa707acc371fbab372ab8992a
Miller <hidden@guess.com>
2025-03-23 12:37:23 +0800

    multy process merge

--- merge_sar.py
+++ merge_sar.py
@@ -4,6 +4,32 @@ from PIL import Image
 from tqdm import tqdm
 
 
+def merge(file, files_B, output_folder):
+    if file not in files_B:
+        print("文件名不匹配 跳过")
+
+    path_A = os.path.join(folder_A, file)
+    path_B = os.path.join(folder_B, file)
+
+    if not (
+        path_A.endswith((".png", ".jpg", ".jpeg", ".tif"))
+        and path_B.endswith((".png", ".jpg", ".jpeg", ".tif"))
+    ):
+        print(f"跳过非图片文件: {file}, {file}")
+
+    # 打开图片
+    img_A = Image.open(path_A)
+    img_B = Image.open(path_B)
+
+    # 拼接
+    merged = Image.new("RGB", (img_A.width + img_B.width, img_A.height))
+    merged.paste(img_A, (0, 0))  # 左边放 A
+    merged.paste(img_B, (img_A.width, 0))  # 右边放 B
+
+    # 保存结果
+    merged.save(os.path.join(output_folder, file))
+
+
 def merge_images(folder_A, folder_B, output_folder):
     os.makedirs(output_folder, exist_ok=True)  # 确保输出文件夹存在
 
@@ -12,30 +38,6 @@ def merge_images(folder_A, folder_B, output_folder):
     files_B = sorted(os.listdir(folder_B))
 
     # for file in tqdm((files_A), total=len(files_A)):
-    def merge(file, files_B, output_folder):
-        if file not in files_B:
-            print("文件名不匹配 跳过")
-
-        path_A = os.path.join(folder_A, file)
-        path_B = os.path.join(folder_B, file)
-
-        if not (
-            path_A.endswith((".png", ".jpg", ".jpeg", ".tif"))
-            and path_B.endswith((".png", ".jpg", ".jpeg", ".tif"))
-        ):
-            print(f"跳过非图片文件: {file}, {file}")
-
-        # 打开图片
-        img_A = Image.open(path_A)
-        img_B = Image.open(path_B)
-
-        # 拼接
-        merged = Image.new("RGB", (img_A.width + img_B.width, img_A.height))
-        merged.paste(img_A, (0, 0))  # 左边放 A
-        merged.paste(img_B, (img_A.width, 0))  # 右边放 B
-
-        # 保存结果
-        merged.save(os.path.join(output_folder, file))
 
     task = []
     with ProcessPoolExecutor() as extc:
